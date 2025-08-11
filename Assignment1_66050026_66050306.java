8bc85826aa8698adc90625575233a7c8a1747b68
Ayaka04mi <kantapornphosaeng@gmail.com>
2025-08-11 11:53:56 +0700

    Create Assignment1_66050026_66050306.java
    start by Tangmay

---
+++ Assignment1_66050026_66050306.java
@@ -0,0 +1,16 @@
+public class Assignment1_66050026_66050306 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
+
+class Create_framebyframe {
+
+    public void createFrame() {
+        
+    }
+
+}
